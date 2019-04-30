dde6a0456e9455f19da7ffc301f1858cecb5d246
Peter Beens <pbeens@gmail.com>
2019-04-30 10:21:08 -0400

    Update Python Stub.py
    Added functional code.

--- Python Stub.py
+++ Python Stub.py
@@ -1,3 +1,9 @@
 # This stub can be used to read a data file in from this repository
 
-URL = '' # <--- enter the URL here
+from urllib.request import urlopen
+
+url = '' # <--- enter the URL here
+
+textFile = urlopen(url).readlines() # adapt for your specific needs >> readline? readlines? read?
+
+print(textFile) # Just to check if the data got read in correctly. Delete once you're satisfied eveything is working okay.
