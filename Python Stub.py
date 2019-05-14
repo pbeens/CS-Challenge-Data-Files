d23fce6f05a6be3ea56e620386787ac1cb3aa3af
Peter Beens <pbeens@gmail.com>
2019-05-14 17:35:08 -0400

    Update Python Stub.py

--- Python Stub.py
+++ Python Stub.py
@@ -1,12 +1,16 @@
-# This stub can be used to read a data file in from this repository
+'''
+This stub can be used to read a data file in from this repository
+'''
 
+# import(s)
 from urllib.request import urlopen
 
-url = 'https://raw.githubusercontent.com/pbeens/CS-Challenge-Data-Files/master/ECOO/2016/Round%202/DATA10.txt' # <--- enter the URL here
+# substitute in the URL you need to read from
+url ='https://raw.githubusercontent.com/pbeens/CS-Challenge-Data-Files/master/ECOO/2016/Round%202/DATA10.txt'
 
-textFile = urlopen(url).read().decode('utf-8') # adapt for your specific needs >> readline? readlines? read?
+# read in and split text file into list of lines
+lines = urlopen(url).readlines() # adapt for your specific needs >> readline? readlines? read?
 
- # Just to check if the data got read in correctly. Delete once you're satisfied everything is working okay.
-lines = textFile.strip().split('\n')
-print(lines)
-print(textFile)
+# let's see what we've got...
+for line in lines:
+    print(line)
